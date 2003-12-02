f2ff28e5c4ad1e17bc6b7679312eb71511b72fa1
Chunky Kibbles <chunky@icculus.org>
2003-12-02 05:59:16 +0000

    Fixed License Comments

--- src/aajm/aa_drawline.c
+++ src/aajm/aa_drawline.c
@@ -1,3 +1,18 @@
+/*
+ * AAJM - AA JuggleMaster
+ * (C) Gary Briggs 2003
+ *
+ * JuggleMaster is  free software; you can redistribute  it and/or modify
+ * it under the  terms of the GNU General  Public License as published
+ * by the Free  Software Foundation; either version 2  of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+ * MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.   See the GNU
+ * General Public License for more details.
+ */ 
+
 #include "./aa_drawline.h"
 
 

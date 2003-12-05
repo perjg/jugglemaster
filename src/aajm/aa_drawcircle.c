ac975569384d7bd7ab9a06f44f8d6b74060e11fa
Chunky Kibbles <chunky@icculus.org>
2003-12-05 04:53:11 +0000

    Added abiltiy to fill circles

--- src/aajm/aa_drawcircle.c
+++ src/aajm/aa_drawcircle.c
@@ -18,7 +18,8 @@
 /* Flagrantly ripped from http://homepage.smc.edu/kennedy_john/BCIRCLE.PDF
 	"A Fast Bresenham Type Algorithm For Drawing Circles" */
 void aa_drawcircle(aa_context *context, int cx, int cy,
-				int r, int color) {
+				int r, int color, int fillcolor) {
+	/* Set fillcolor to -1 to leave empty */
 	int x, y, xchange, ychange, radiuserror;
 	x = r;
 	y = 0;
@@ -26,7 +27,7 @@ void aa_drawcircle(aa_context *context, int cx, int cy,
 	ychange = 1;
 	radiuserror = 0;
 	while (x>=y) {
-		aa_plot8circlepoints(context, x, y, cx, cy, color);
+		aa_plot8circlepoints(context, x, y, cx, cy, color, fillcolor);
 		y++;
 		radiuserror+=ychange;
 		ychange+=2;
@@ -44,7 +45,7 @@ void aa_drawcircle(aa_context *context, int cx, int cy,
  * For internal use, Use aa_drawcircle instead.
  */
 void aa_plot8circlepoints(aa_context *context, int x, int y,
-				int cx, int cy, int color) {
+				int cx, int cy, int color, int fillcolor) {
 	aa_putpixel(context, cx+x, cy+y, color);
 	aa_putpixel(context, cx-x, cy+y, color);
 	aa_putpixel(context, cx-x, cy-y, color);
@@ -53,4 +54,8 @@ void aa_plot8circlepoints(aa_context *context, int x, int y,
 	aa_putpixel(context, cx-y, cy+x, color);
 	aa_putpixel(context, cx-y, cy-x, color);
 	aa_putpixel(context, cx+y, cy-x, color);
+	if(fillcolor != -1) {
+		aa_drawline(context, cx-x+1, cy+y, cx+x-1, cy+y, fillcolor);
+		aa_drawline(context, cx-x+1, cy-y, cx+x-1, cy-y, fillcolor);
+	}
 }
--- src/aajm/aa_drawcircle.h
+++ src/aajm/aa_drawcircle.h
@@ -20,15 +20,16 @@
 
 /* Flagrantly ripped from http://homepage.smc.edu/kennedy_john/BCIRCLE.PDF
         "A Fast Bresenham Type Algorithm For Drawing Circles" */
+/* Set fillcolor to -1 to leave circle hollow */
 void aa_drawcircle(aa_context *context, int cx, int cy,
-			int r, int color);
+			int r, int color, int fillcolor);
 
 /*
  * Draws 8 of the points on the circle radius
  * For internal use, Use aa_drawcircle instead.
  */
 void aa_plot8circlepoints(aa_context *context, int x, int y,
-			int cx, int cy, int color);
+			int cx, int cy, int color, int fillcolor);
 
 
 #endif

3666f4381db13e11ff0235cf28ffa888cf538f46
Per Johan Groland <per@akafugu.jp>
2006-10-03 09:02:25 +0000

    Initial add of jmlib-js (jmlib JavaScript port)

---
+++ src/jmlib-js/jugglemaster.js
@@ -0,0 +1,254 @@
+// 	$Id: validator.cpp 208 2006-08-04 01:40:24Z per $	 
+
+/*
+ * jmlib-js - Portable JuggleMaster Library (JavaScript Version)
+ * Version 1.0
+ * (C) Per Johan Groland 2006
+ *
+ * Based on JMLib 2.0, (C) Per Johan Groland and Gary Briggs
+ *
+ * Based on JuggleMaster Version 1.60
+ * Copyright (c) 1995-1996 Ken Matsuoka
+ *
+ * You may redistribute and/or modify JMLib_js under the terms of the
+ * Modified BSD License as published in various places online or in the
+ * COPYING.jmlib file in the package you downloaded.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+ * MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * Modified BSD License for more details.
+ */ 
+
+var canvas, ctx;
+var jmlib;
+
+function log(s) {
+  // Safari
+  //if (window.console) { window.console.log(s); }
+  // Opera
+  //opera.postError(s);
+}
+
+function getArgs() {
+  var args = [];
+  var query = location.search.substring(1);
+  if (query == null) { return null; }    
+  var pairs = query.split("&");
+  
+  for (var i = 0; i < pairs.length; i++) {
+    var pos = pairs[i].indexOf("=");
+    if (pos == -1) { continue; }
+    var argname = pairs[i].substring(0, pos);
+    var value   = pairs[i].substring(pos+1);
+    value = decodeURIComponent(value);
+    
+    args[argname] = value;
+  }
+  return args;
+}
+
+function loop() {
+  jmlib.doJuggle();  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);	
+
+/*
+  // draw grid
+  ctx.lineWidth = "0.2";	
+  ctx.fillStyle = "gray";	
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.closePath();
+  ctx.stroke()
+
+  ctx.beginPath();
+  ctx.moveTo(canvas.width, 0);
+  ctx.lineTo(0, canvas.height);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height/2);
+  ctx.lineTo(canvas.width, canvas.height/2);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(canvas.width/2, 0);
+  ctx.lineTo(canvas.width/2, canvas.height);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.lineWidth = "2.0";
+  ctx.beginPath();
+  ctx.moveTo(pos++, 3);
+  ctx.lineTo(pos+3, 3);
+  ctx.closePath();
+  ctx.stroke()
+*/
+
+  ctx.lineWidth = "1.0";	
+
+  // draw head
+  ctx.beginPath();
+  ctx.moveTo(jmlib.ap.hx+jmlib.ap.hr, jmlib.ap.hy);
+  ctx.arc(jmlib.ap.hx, jmlib.ap.hy, jmlib.ap.hr, 0, Math.PI*2, false);
+  ctx.closePath();
+  ctx.stroke();
+
+  // draw arms
+  for (var i=0;i<5;i++) {
+    ctx.beginPath();
+    ctx.moveTo(jmlib.ap.rx[i], jmlib.ap.ry[i]); 
+    ctx.lineTo(jmlib.ap.rx[i+1], jmlib.ap.ry[i+1]);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(jmlib.ap.lx[i], jmlib.ap.ly[i]); 
+    ctx.lineTo(jmlib.ap.lx[i+1], jmlib.ap.ly[i+1]);
+    ctx.closePath();
+    ctx.stroke();
+  }
+	
+  // draw hands
+  for (var i=0; i < 10; i++) {
+    ctx.beginPath();
+    ctx.moveTo(jmlib.rhand.gx + jmlib.handpoly.rx[i],        jmlib.rhand.gy + jmlib.handpoly.ry[i]);
+    ctx.lineTo(jmlib.rhand.gx + jmlib.handpoly.rx[(i+1)%10], jmlib.rhand.gy + jmlib.handpoly.ry[(i+1)%10]);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(jmlib.lhand.gx + jmlib.handpoly.lx[i],        jmlib.lhand.gy + jmlib.handpoly.ly[i]);
+    ctx.lineTo(jmlib.lhand.gx + jmlib.handpoly.lx[(i+1)%10], jmlib.lhand.gy + jmlib.handpoly.ly[(i+1)%10]);
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+  // draw balls	
+  var r = jmlib.getBallRadius();
+  for (var i = 0; i < jmlib.numBalls(); i++) {
+    var ball = jmlib.b[i];
+  
+    ctx.beginPath();
+    ctx.moveTo(ball.gx+r+r, ball.gy);
+    ctx.arc(ball.gx+r, ball.gy+r, r, 0, Math.PI*2, false);
+    ctx.fillStyle = getColor(i);
+    ctx.fill();
+    ctx.closePath();	  
+	}
+
+  setTimeout(loop, 10);
+}
+
+function getColor(i) {
+  switch(i%10) {
+    case 0:
+	  return "red";
+    case 1:
+	  return "blue";
+    case 2:
+	  return "green";
+    case 3:
+	  return "yellow";
+    case 4:
+	  return "pink";
+    case 5:
+	  return "purple";
+    case 6:
+	  return "magenta";
+    case 7:
+	  return "red";
+    case 8:
+	  return "red";
+    case 9:
+	  return "red";
+	default:
+	  return "red";
+  }
+}
+
+function load() {
+  var args = getArgs();
+  var site = null;
+  var style = null;
+  
+  if (args != null) {
+    var site = args["site"];
+    var style = args["style"];
+  }
+
+  if (site == null)  { site = "3"; }
+  if (style == null) { style = "Normal"; }
+
+  canvas = document.getElementById("cv");
+  ctx = canvas.getContext("2d");
+
+  cb = function(err) { alert(err); }
+
+  jmlib = new JMLib(cb);
+  jmlib.setScalingMethod(JMLib.SCALING_METHOD_DYNAMIC);
+  jmlib.setWindowSize(canvas.width, canvas.height);
+  jmlib.setPattern(site, site, jmlib.height_ratio, jmlib.dwell_ratio);
+  jmlib.setStyleEx(style);
+  jmlib.startJuggle();
+
+  loop();
+}
+
+function load_widget() {
+  //fixme: read from cookie here
+  site = "3";
+  style = "Normal";
+
+  canvas = document.getElementById("cv");
+  ctx = canvas.getContext("2d");
+
+  //fixme: callback doesn't work
+  //cb = function(err) { alert(err); }
+
+  jmlib = new JMLib(null);
+  jmlib.setScalingMethod(JMLib.SCALING_METHOD_DYNAMIC);
+  jmlib.setWindowSize(canvas.width, canvas.height);
+  jmlib.setPattern(site, site, jmlib.height_ratio, jmlib.dwell_ratio);
+  jmlib.setStyleEx(style);
+  jmlib.startJuggle();
+
+  loop();
+}
+
+function changeSite(site) {
+  if (jmlib.siteswap == site) {
+    return true;
+  }
+
+  if (!JMValidator.validateSite(site)) {
+    jmlib.lastError = "Invalid siteswap";
+    return false;
+  }
+
+  jmlib.stopJuggle();
+  var ret = jmlib.setPattern(site, site, jmlib.height_ratio, jmlib.dwell_ratio);
+  jmlib.startJuggle();
+  
+  return ret;
+}
+
+function validateSite(site) {
+  return JMValidator.validateSite(site);
+}
+
+
+function changeStyle(style) {
+  return jmlib.setStyleEx(style);
+}
+
+function getLastError() {
+  return jmlib.lastError;
+}
+
+function getCurrentSite() {
+  return jmlib.siteswap;
+}

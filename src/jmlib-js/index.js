ad2e9eca204ede341fab213bd99f1d8edbc1ff4f
Per Johan Groland <per@akafugu.jp>
2016-04-27 13:55:20 +0900

    Porting jmlib-js as a npm package. Initial setup

---
+++ src/jmlib-js/index.js
@@ -0,0 +1,20 @@
+/*
+ * jmlib-js - Portable JuggleMaster Library (JavaScript Version)
+ * Version 2.0
+ * (C) Per Johan Groland 2006-2016
+ *
+ * Based on JMLib 2.0, (C) Per Johan Groland and Gary Briggs
+ *
+ * Based on JuggleMaster Version 1.60
+ * Copyright (c) 1995-1996 Ken Matsuoka
+ *
+ * You may redistribute and/or modify JMLib_js under the terms of the
+ * Modified BSD License as published in various places online or in the
+ * COPYING.jmlib file in the package you downloaded.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+ * MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * Modified BSD License for more details.
+ */
+
